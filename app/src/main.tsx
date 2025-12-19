35ecf861e4728493d2ae3f6e8da9242614fcd4a2
Neon-099 <Emmannatividad099@gmail.com>
2025-12-19 18:06:54 +0800

    routing

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MedicationProvider } from './context/MedicationContext'
+
+import Home from './pages/users/Home'
+import MedicationList from './pages/users/MedicationList';
+import AddMedication from './pages/users/AddMedication'
+import EditMedication from './pages/users/EditMedication'
+import MedicationDetails from './pages/users/MedicationDetails'
+import Compliance from './pages/users/Compliance'
+import Alerts from './pages/users/Alerts'
+import Settings from './pages/users/Settings'
+import Navigation from './components/Navigation'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <MedicationProvider>
+      <div className="pb-20">
+        <Routes>
+          <Route path="/" element={<Home /> }/>
+          <Route path='/medications' element={ <MedicationList /> }/>
+          <Route path="/medications/add" element={<AddMedication />} />
+          <Route path="/medications/:id" element={<MedicationDetails />} />
+          <Route path="/medications/:id/edit" element={<EditMedication />} />
+          <Route path="/compliance" element={<Compliance />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+      <Navigation />
+    </MedicationProvider>
+  </BrowserRouter>
 )
